42f97259e6a9dedc0d8d660a6e0fe626d602ed27
clebsonXpaty <clbsonalves@gmail.com>
2023-02-09 11:11:41 -0300

    Diretório Java adicionado ao repositório

---
+++ sintaxa_variaveis_fluxo/src/sintaxa_variaveis_fluxo/testa_variaveis.java
@@ -0,0 +1,18 @@
+package sintaxa_variaveis_fluxo;
+
+public class testa_variaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá novo teste!");
+		int idade;
+		idade = 37;
+		System.out.println(idade);
+		idade = idade + 10;
+		System.out.println(idade);
+		idade = idade * 2 -10;
+		System.out.println("sua idade é: " + idade);
+
+	}
+
+}
