e785ad1d1e9c1287b3aa17e314ad50aa55aac955
clebsonXpaty <clbsonalves@gmail.com>
2023-02-17 10:36:33 -0300

    foi criado um novo projeto (MeuBancoComposto), foram criadas novas classes de testes no dois projetos existentes

--- MeuBanco/src/TestaMetodo.java
+++ MeuBanco/src/TestaMetodo.java
@@ -1,13 +1,31 @@
 
 public class TestaMetodo {
 	public static void main(String[] args) {
-		
-	
-	
-	Conta contaDoClebson = new Conta();
-	contaDoClebson.saldo = 100;
-	System.out.println(contaDoClebson.saldo);
-	contaDoClebson.deposita(50);
-	System.out.println(contaDoClebson.saldo);
+
+		Conta contaDoClebson = new Conta();
+		contaDoClebson.saldo = 100;
+		System.out.println(contaDoClebson.saldo);
+		contaDoClebson.deposita(50);
+		System.out.println(contaDoClebson.saldo);
+		// uso de método, cada método precisa de um parametro para passar valores
+		contaDoClebson.saca(30);
+		System.out.println(contaDoClebson.saldo);
+		// Testando se o valor foi retido da conta ou não
+		boolean confereSaldo = contaDoClebson.saca(200);
+		System.out.println(confereSaldo);
+
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+
+		contaDaMarcela.transfere(300, contaDoClebson);
+		System.out.println("Saldo da conta da Mercela: " + contaDaMarcela.saldo);
+		System.out.println("Saldo da conta do Clebson: " + contaDoClebson.saldo);
+
+		boolean sucessoTransferencia;
+		if (sucessoTransferencia = contaDaMarcela.transfere(300, contaDoClebson)) {
+			System.out.println("Transferência ralizada com sucesso");
+		} else {
+			System.out.println("Saldo insuficiente, não foi possível realizar " + "essa operação!");
+		}
 	}
 }
